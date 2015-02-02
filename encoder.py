4744aaf6c226a5df3e2a44005e99737f517f5641
PTC <pheartheceal@gmail.com>
2015-02-01 22:33:51 -0800

    some encoding, lol

--- encoder.py
+++ encoder.py
@@ -1,65 +1,67 @@
-"""Encodes a given string in Base64. 
-   By Crispin A. Stichart."""
+import string
+
+BS64 = string.ascii_uppercase + string.ascii_lowercase + string.digits + "+/"
+
 
 def encode(string):
     """Takes a string and returns the Base64 representation."""
-    binary = convert_to_binary(string)
-    # Figures out how much padding the string needs, if any
-    if len(user_input) % 3 != 0:
-        if (len(user_input)+1) % 3 == 0:
-            binary = binary << 2
-            padding = 1
-        else: 
-            binary = binary << 4
-            padding = 2
-    else: padding = 0
-    print("Binary after", len(bin(binary))-1)
-    # The leadingzero part is because convert_to_binary() can return
-    # an odd number if the first character is a number, because 
-    # ord() of any digit is a 7 bit integer. 
-    # The "- (1 - leadingzero)" is because bin returns a string
-    # that includes the letter b.
-    # chars is how many 6-bit chunks there are in the string
-    leadingzero = (len(bin(binary)) - 1) % 2
-    chars = (len(bin(binary)) - (1 - leadingzero)) // 6 
-    b64 = "" # converted characters are placed in this string
-    # the main conversion loop
-    for char in range(chars-1, -1, -1): #counts down in reverse
-        code = binary >> (6 * char)
-        binary = binary - (code << (6*char))
-        if 0 <= code <= 25:
-            b64 += chr(code + ord("A"))
-        if 26 <= code <= 51:
-            b64 += chr(code + ord("a") - 26)
-        if 52 <= code <= 61:
-            b64 += chr(code + ord("0") - 52)
-        if code == 62:
-            b64 += "+"   
-        if code == 63:
-            b64 += "/"
-    b64 += "=" * padding
-    return b64
+
+    # Get bytes of string
+    str_bytes = bytes(string, "utf-8")
+    return encode_bytes(str_bytes)
+
+def encode_bytes(str_bytes):
+    """Takes bytes and returns Base64 encoding."""
+
+    # Get bytes as an int
+    str_int = b_to_i(str_bytes)
+
+    # keeping track of where we are
+    num_bytes = len(str_bytes)
+    chunks = num_bytes // 3
+    res = ""
+
+    # The mask to get the 6 bits we want.
+    # Start it on the left end.
+    bits_left = 8 * num_bytes - 6
+    mask = 63 << bits_left
+    for _ in range(4 * chunks):
+        # convert the next 3 bytes into 4 base-64 chars
+        num = (str_int & mask) >> bits_left
+        res += BS64[num]
+        bits_left -= 6
+        mask = mask >> 6
+
+    # get stuff we missed
+    num_missed_bytes = num_bytes % 3
+    if num_missed_bytes:
+        missed_bytes = str_bytes[-num_missed_bytes:]
+        missed_bytes += b"\x00" * (-num_bytes % 3)
+        res += encode_bytes(missed_bytes)
+
+    # handle padding
+    num_padding = -num_bytes % 3
+    res = res[:len(res) - num_padding] + "=" * num_padding
+
+    return res
 
 
-def convert_to_binary(string):
-    """Takes a string and packs each letter into an integer,
-       then returns that number."""
-    binary = 0
-    for letter in string:
-        # okay, so what's going on here is that some characters oridinal values
-        # are less than 8 bits. So this makes sure they're padded correctly. 
-        code = ord(letter)
-        bitnumber = len(bin(code))-1 # bin() returns string with "b" in it
-        print("bitnumber", bitnumber)
-        binary = code + (binary << (8))
-    return binary
+def b_to_i(bytes):
+    """Converts bytes to an int"""
+    res = 0
+    for b in bytes:
+        res = res << 8
+        res = res | b
+    return res
 
 if __name__ == "__main__":
-    print("Give me a line of text, and I'll give you the Base 64 representation!")
+    print(
+        "Give me a line of text, and I'll give you the Base 64 representation!")
     print("Give me a blank line (Just press enter!) to quit me.")
     while True:
         user_input = input(">>> ")
-        if user_input == "": break
+        if user_input == "":
+            break
         print(encode(user_input))
         print("")
-    print("Bye!") 
+    print("Bye!")
